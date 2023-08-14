f78fbfe766c8f5d2f7096184a800c397e29ca4c1
erdem <erdm@disroot.org>
2023-08-14 21:57:38 +0300

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <header>
               <div className="flex items-center justify-between">
                 <ModeToggle />
-                <nav className="ml-auto text-sm font-medium space-x-6">
-                  <Link href="/">Home</Link>
-                  <Link href="/about">About</Link>
-                </nav>
               </div>
             </header>
             <main>{children}</main>
