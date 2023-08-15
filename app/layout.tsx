d993949a057bddb00dee8d0e1352fdad4a5aee1b
erdem <erdm@disroot.org>
2023-08-15 11:26:28 +0300

    url

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { ModeToggle } from "@/components/mode-toggle"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "blog.erdm.us",
+  title: "blog.erdm.lol",
   description: "Erdem Hacisalihoglu's blog",
 }
 
